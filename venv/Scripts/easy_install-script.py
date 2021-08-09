68410e6cf5349fb5fb984dae2be8619fb0e24f28
julian0808 <41093464+julian0808@users.noreply.github.com>
2021-08-09 14:35:25 -0500

    First Commit

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\julia\Desktop\UDEMY + other courses\YouTube\Python Selenium Tutorial #1 - Web Scraping, Bots & Testing\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.0.1','console_scripts','easy_install'
+__requires__ = 'setuptools==39.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.0.1', 'console_scripts', 'easy_install')()
+    )
